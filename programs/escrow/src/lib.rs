93540ff8b97b76561505d0c4b57faf36130ba338
Eduard <eduard.daniel.toma@gmail.com>
2024-07-30 03:22:27 +0300

    added escrow basic project template

---
+++ programs/escrow/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("7otJxPBv9mFGwVZ3UtSQMY5sFSmDjQPXX5XKuVY7C9bm");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
